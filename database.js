5041d0a5dc4665c00abdae3e5e880376cda2c11c
BiagettiLuigino <biagetti.luigino@gmail.com>
2026-02-16 19:06:28 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 // Elenco aggiornato delle canzoni e spartiti
 const listaFile = [
     "Lascia i tuoi pesi.mp3",
-    "Mano potente.mp3",
+    "Mano Potente.mp3",
     "Santo Gen come fuoco vivo.mp3",
     "Stai con me - Luce RnS 2015.mp3",
     "Astro del ciel in SOL.mpeg",
     "E' nato dio per noi.mpeg",
     "Gloria Mariano.mpeg",
-    "il seme del tuo campo.mpeg",
+    "Il seme del tuo campo.mpeg",
     "Santo MJ.mpeg",
     "Sono qui a lodarti.mpeg"
 ];
